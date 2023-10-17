961f4aefd9a032b95850d46a5dd0065dbe99ab45
Chris Heise <111378122+cheisenmhu@users.noreply.github.com>
2023-10-17 12:15:09 -0600

    Add My Initial Code
    Added the code I already had for the second page, app.py, and updated gitignore to include python byte data.

---
+++ app.py
@@ -0,0 +1,9 @@
+# Programmers: Anita Martin, Chris Heise, Dion Boyer, and Felix Jaramillo
+# Course: BSSD 4350 - Agile Methodologies
+# Instructor: Jonathan Lee
+# Program: Inclusivity Editor App
+# File: app.py
+
+from second_page import second_page
+
+second_page.launch()
